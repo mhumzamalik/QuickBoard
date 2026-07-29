1797e8dd61a09d52f7f6b8d06e5367d5b8976144
Muhammad Hamza <46humza@gmail.com>
2026-07-29 11:57:52 +0500

    Use GitHub release downloads

--- src/lib/downloadLinks.ts
+++ src/lib/downloadLinks.ts
@@ -1,5 +1,10 @@
 export const DOWNLOAD_LINKS = {
-  mobileApp: '/downloads/QuickBoard.apk',
-  desktopApp: '/downloads/QuickBoard_1.0.0_x64-setup.exe',
-  chromeExtension: '/downloads/dist.crx',
+  mobileApp:
+    "https://github.com/mhumzamalik/QuickBoard/releases/download/v1.0.0/QuickBoard.apk",
+
+  desktopApp:
+    "https://github.com/mhumzamalik/QuickBoard/releases/download/v1.0.0/QuickBoard_1.0.0_x64-setup.exe",
+
+  chromeExtension:
+    "https://github.com/mhumzamalik/QuickBoard/releases/download/v1.0.0/dist.crx",
 } as const;
