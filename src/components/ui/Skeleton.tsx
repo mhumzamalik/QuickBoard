7909fecb8e433bf7f7c8231352786e7d6a4e2425
Muhammad Hamza <46humza@gmail.com>
2026-07-29 15:20:23 +0500

    Remove all files from production

--- src/components/ui/Skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Skeleton: React.FC<{ className?: string }> = ({ className = '' }) => {
-  return (
-    <div
-      className={`animate-pulse rounded-lg bg-slate-200 dark:bg-slate-800 ${className}`}
-    />
-  );
-};
